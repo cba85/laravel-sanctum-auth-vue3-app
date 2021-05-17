40cd0280fd02d245e8fd7c92c53211ea14830247
Clement Barbaza <clement.barbaza@gmail.com>
2021-05-17 12:57:27 +0200

    pwa icons + splash

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: "To Don't List",
+    appleMobileWebAppCapable: "yes",
+    iconPaths: {
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png'
+    }
+  },
+};
